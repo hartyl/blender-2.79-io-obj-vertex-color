8d41bbff85a0dc862158bed7e095ce1467ac9904
HARTYL YHEA BOI my heart is not broken <101079305+hartyl@users.noreply.github.com>
2025-03-01 16:36:18 -0300

    Export now available for version 2.79

--- io_scene_obj_with_vc/__init__.py
+++ io_scene_obj_with_vc/__init__.py
@@ -19,17 +19,16 @@
 # <pep8-80 compliant>
 
 bl_info = {
-    "name": "Wavefront OBJ format with Vertex Color Support",
-    "author": "Tomoaki Osada, Campbell Barton, Bastien Montagne",
-    "version": (0, 0, 1),
-    "blender": (2, 81, 6),
+    "name": "Wavefront OBJ format with Vertex Color",
+    "author": "Tomoaki Osada, Campbell Barton, Bastien Montagne, Hartyl",
+    "version": (2, 3, 7),
+    "blender": (2, 78, 0),
     "location": "File > Import-Export",
-    "description": "Import-Export OBJ with Vertex Color, Import OBJ mesh, UV's, materials and textures",
+    "description": "Import-Export OBJ, Import OBJ mesh, UV's, materials, textures and Colors~",
     "warning": "",
-    "doc_url": "https://github.com/ynyBonfennil/blender-io-obj-vertex-color",
+    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Import-Export/Wavefront_OBJ",
     "support": 'COMMUNITY',
-    "category": "Import-Export",
-}
+    "category": "Import-Export"}
 
 if "bpy" in locals():
     import importlib
@@ -49,68 +48,70 @@ from bpy.props import (
 from bpy_extras.io_utils import (
         ImportHelper,
         ExportHelper,
-        orientation_helper,
+        orientation_helper_factory,
         path_reference_mode,
         axis_conversion,
         )
 
 
-@orientation_helper(axis_forward='-Z', axis_up='Y')
-class ImportOBJ(bpy.types.Operator, ImportHelper):
+IOOBJOrientationHelper = orientation_helper_factory("IOOBJOrientationHelper", axis_forward='-Z', axis_up='Y')
+
+
+class ImportOBJ(bpy.types.Operator, ImportHelper, IOOBJOrientationHelper):
     """Load a Wavefront OBJ File"""
     bl_idname = "import_scene.obj"
-    bl_label = "Import OBJ with VC"
+    bl_label = "Import OBJ"
     bl_options = {'PRESET', 'UNDO'}
 
     filename_ext = ".obj"
-    filter_glob: StringProperty(
+    filter_glob = StringProperty(
             default="*.obj;*.mtl",
             options={'HIDDEN'},
             )
 
-    use_edges: BoolProperty(
+    use_edges = BoolProperty(
             name="Lines",
             description="Import lines and faces with 2 verts as edge",
             default=True,
             )
-    use_smooth_groups: BoolProperty(
+    use_smooth_groups = BoolProperty(
             name="Smooth Groups",
             description="Surround smooth groups by sharp edges",
             default=True,
             )
 
-    use_split_objects: BoolProperty(
+    use_split_objects = BoolProperty(
             name="Object",
             description="Import OBJ Objects into Blender Objects",
             default=True,
             )
-    use_split_groups: BoolProperty(
+    use_split_groups = BoolProperty(
             name="Group",
             description="Import OBJ Groups into Blender Objects",
-            default=False,
+            default=True,
             )
 
-    use_groups_as_vgroups: BoolProperty(
+    use_groups_as_vgroups = BoolProperty(
             name="Poly Groups",
             description="Import OBJ groups as vertex groups",
             default=False,
             )
 
-    use_image_search: BoolProperty(
+    use_image_search = BoolProperty(
             name="Image Search",
             description="Search subdirs for any associated images "
                         "(Warning, may be slow)",
             default=True,
             )
 
-    split_mode: EnumProperty(
+    split_mode = EnumProperty(
             name="Split",
             items=(('ON', "Split", "Split geometry, omits unused verts"),
                    ('OFF', "Keep Vert Order", "Keep vertex order from file"),
                    ),
             )
 
-    global_clamp_size: FloatProperty(
+    global_clamp_size = FloatProperty(
             name="Clamp Size",
             description="Clamp bounds under this value (zero to disable)",
             min=0.0, max=1000.0,
@@ -138,224 +139,162 @@ class ImportOBJ(bpy.types.Operator, ImportHelper):
                                         from_up=self.axis_up,
                                         ).to_4x4()
         keywords["global_matrix"] = global_matrix
+        keywords["use_cycles"] = (context.scene.render.engine == 'CYCLES')
 
-        if bpy.data.is_saved and context.preferences.filepaths.use_relative_paths:
+        if bpy.data.is_saved and context.user_preferences.filepaths.use_relative_paths:
             import os
             keywords["relpath"] = os.path.dirname(bpy.data.filepath)
 
         return import_obj.load(context, **keywords)
 
     def draw(self, context):
-        pass
-
-
-class OBJ_PT_import_include(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Include"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "IMPORT_SCENE_OT_obj"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.prop(operator, 'use_image_search')
-        layout.prop(operator, 'use_smooth_groups')
-        layout.prop(operator, 'use_edges')
-
-
-class OBJ_PT_import_transform(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Transform"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "IMPORT_SCENE_OT_obj"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.prop(operator, "global_clamp_size")
-        layout.prop(operator, "axis_forward")
-        layout.prop(operator, "axis_up")
-
-
-class OBJ_PT_import_geometry(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Geometry"
-    bl_parent_id = "FILE_PT_operator"
-    bl_options = {'DEFAULT_CLOSED'}
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "IMPORT_SCENE_OT_obj"
-
-    def draw(self, context):
         layout = self.layout
 
-        sfile = context.space_data
-        operator = sfile.active_operator
+        row = layout.row(align=True)
+        row.prop(self, "use_smooth_groups")
+        row.prop(self, "use_edges")
 
-        layout.row().prop(operator, "split_mode", expand=True)
+        box = layout.box()
+        row = box.row()
+        row.prop(self, "split_mode", expand=True)
 
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        col = layout.column()
-        if operator.split_mode == 'ON':
-            col.prop(operator, "use_split_objects", text="Split by Object")
-            col.prop(operator, "use_split_groups", text="Split by Group")
+        row = box.row()
+        if self.split_mode == 'ON':
+            row.label(text="Split by:")
+            row.prop(self, "use_split_objects")
+            row.prop(self, "use_split_groups")
         else:
-            col.prop(operator, "use_groups_as_vgroups")
+            row.prop(self, "use_groups_as_vgroups")
+
+        row = layout.split(percentage=0.67)
+        row.prop(self, "global_clamp_size")
+        layout.prop(self, "axis_forward")
+        layout.prop(self, "axis_up")
+
+        layout.prop(self, "use_image_search")
+        layout.prop(operator, 'use_colors')
 
 
-@orientation_helper(axis_forward='-Z', axis_up='Y')
-class ExportOBJ(bpy.types.Operator, ExportHelper):
+class ExportOBJ(bpy.types.Operator, ExportHelper, IOOBJOrientationHelper):
     """Save a Wavefront OBJ File"""
 
     bl_idname = "export_scene.obj"
-    bl_label = 'Export OBJ with VC'
+    bl_label = 'Export OBJ'
     bl_options = {'PRESET'}
 
     filename_ext = ".obj"
-    filter_glob: StringProperty(
+    filter_glob = StringProperty(
             default="*.obj;*.mtl",
             options={'HIDDEN'},
             )
 
     # context group
-    use_selection: BoolProperty(
+    use_selection = BoolProperty(
             name="Selection Only",
             description="Export selected objects only",
             default=False,
             )
-    use_animation: BoolProperty(
+    use_animation = BoolProperty(
             name="Animation",
             description="Write out an OBJ for each frame",
             default=False,
             )
 
     # object group
-    use_mesh_modifiers: BoolProperty(
+    use_mesh_modifiers = BoolProperty(
             name="Apply Modifiers",
             description="Apply modifiers",
             default=True,
             )
-    # Non working in Blender 2.8 currently.
-    # ~ use_mesh_modifiers_render: BoolProperty(
-            # ~ name="Use Modifiers Render Settings",
-            # ~ description="Use render settings when applying modifiers to mesh objects",
-            # ~ default=False,
-            # ~ )
+    use_mesh_modifiers_render = BoolProperty(
+            name="Use Modifiers Render Settings",
+            description="Use render settings when applying modifiers to mesh objects",
+            default=False,
+            )
 
     # extra data group
-    use_edges: BoolProperty(
+    use_edges = BoolProperty(
             name="Include Edges",
             description="",
             default=True,
             )
-    use_smooth_groups: BoolProperty(
+    use_smooth_groups = BoolProperty(
             name="Smooth Groups",
             description="Write sharp edges as smooth groups",
             default=False,
             )
-    use_smooth_groups_bitflags: BoolProperty(
+    use_smooth_groups_bitflags = BoolProperty(
             name="Bitflag Smooth Groups",
             description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
                         "(produces at most 32 different smooth groups, usually much less)",
             default=False,
             )
-    use_normals: BoolProperty(
+    use_normals = BoolProperty(
             name="Write Normals",
             description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
             default=True,
             )
-    use_colors: BoolProperty(
-            name="Write Vertex Colors",
-            description="Write out the active vertex color layer",
+    use_colors = BoolProperty(
+            name="Include Vertex Colors",
+            description="Add RGB channel to the position attribute",
             default=True,
-    )
-    use_uvs: BoolProperty(
+            )
+    use_uvs = BoolProperty(
             name="Include UVs",
             description="Write out the active UV coordinates",
             default=True,
             )
-    use_materials: BoolProperty(
+    use_materials = BoolProperty(
             name="Write Materials",
             description="Write out the MTL file",
             default=True,
             )
-    use_triangles: BoolProperty(
+    use_triangles = BoolProperty(
             name="Triangulate Faces",
             description="Convert all faces to triangles",
             default=False,
             )
-    use_nurbs: BoolProperty(
+    use_nurbs = BoolProperty(
             name="Write Nurbs",
             description="Write nurbs curves as OBJ nurbs rather than "
                         "converting to geometry",
             default=False,
             )
-    use_vertex_groups: BoolProperty(
+    use_vertex_groups = BoolProperty(
             name="Polygroups",
             description="",
             default=False,
             )
 
     # grouping group
-    use_blen_objects: BoolProperty(
-            name="OBJ Objects",
-            description="Export Blender objects as OBJ objects",
+    use_blen_objects = BoolProperty(
+            name="Objects as OBJ Objects",
+            description="",
             default=True,
             )
-    group_by_object: BoolProperty(
-            name="OBJ Groups",
-            description="Export Blender objects as OBJ groups",
+    group_by_object = BoolProperty(
+            name="Objects as OBJ Groups ",
+            description="",
             default=False,
             )
-    group_by_material: BoolProperty(
+    group_by_material = BoolProperty(
             name="Material Groups",
-            description="Generate an OBJ group for each part of a geometry using a different material",
+            description="",
             default=False,
             )
-    keep_vertex_order: BoolProperty(
+    keep_vertex_order = BoolProperty(
             name="Keep Vertex Order",
             description="",
             default=False,
             )
 
-    global_scale: FloatProperty(
+    global_scale = FloatProperty(
             name="Scale",
             min=0.01, max=1000.0,
             default=1.0,
             )
 
-    path_mode: path_reference_mode
+    path_mode = path_reference_mode
 
     check_extension = True
 
@@ -370,7 +309,7 @@ class ExportOBJ(bpy.types.Operator, ExportHelper):
                                             "filter_glob",
                                             ))
 
-        global_matrix = (Matrix.Scale(self.global_scale, 4) @
+        global_matrix = (Matrix.Scale(self.global_scale, 4) *
                          axis_conversion(to_forward=self.axis_forward,
                                          to_up=self.axis_up,
                                          ).to_4x4())
@@ -378,125 +317,18 @@ class ExportOBJ(bpy.types.Operator, ExportHelper):
         keywords["global_matrix"] = global_matrix
         return export_obj.save(context, **keywords)
 
-    def draw(self, context):
-        pass
-
-
-class OBJ_PT_export_include(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Include"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "EXPORT_SCENE_OT_obj"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        col = layout.column(heading="Limit to")
-        col.prop(operator, 'use_selection')
-
-        col = layout.column(heading="Objects as", align=True)
-        col.prop(operator, 'use_blen_objects')
-        col.prop(operator, 'group_by_object')
-        col.prop(operator, 'group_by_material')
-
-        layout.separator()
-
-        layout.prop(operator, 'use_animation')
-
-
-class OBJ_PT_export_transform(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Transform"
-    bl_parent_id = "FILE_PT_operator"
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "EXPORT_SCENE_OT_obj"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.prop(operator, 'global_scale')
-        layout.prop(operator, 'path_mode')
-        layout.prop(operator, 'axis_forward')
-        layout.prop(operator, 'axis_up')
-
-
-class OBJ_PT_export_geometry(bpy.types.Panel):
-    bl_space_type = 'FILE_BROWSER'
-    bl_region_type = 'TOOL_PROPS'
-    bl_label = "Geometry"
-    bl_parent_id = "FILE_PT_operator"
-    bl_options = {'DEFAULT_CLOSED'}
-
-    @classmethod
-    def poll(cls, context):
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        return operator.bl_idname == "EXPORT_SCENE_OT_obj"
-
-    def draw(self, context):
-        layout = self.layout
-        layout.use_property_split = True
-        layout.use_property_decorate = False  # No animation.
-
-        sfile = context.space_data
-        operator = sfile.active_operator
-
-        layout.prop(operator, 'use_mesh_modifiers')
-        # Property definition disabled, not working in 2.8 currently.
-        # layout.prop(operator, 'use_mesh_modifiers_render')
-        layout.prop(operator, 'use_smooth_groups')
-        layout.prop(operator, 'use_smooth_groups_bitflags')
-        layout.prop(operator, 'use_normals')
-        layout.prop(operator, 'use_colors')
-        layout.prop(operator, 'use_uvs')
-        layout.prop(operator, 'use_materials')
-        layout.prop(operator, 'use_triangles')
-        layout.prop(operator, 'use_nurbs', text="Curves as NURBS")
-        layout.prop(operator, 'use_vertex_groups')
-        layout.prop(operator, 'keep_vertex_order')
-
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportOBJ.bl_idname, text="Wavefront with VC (.obj)")
+    self.layout.operator(ImportOBJ.bl_idname, text="Wavefront (.obj)")
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportOBJ.bl_idname, text="Wavefront with VC (.obj)")
+    self.layout.operator(ExportOBJ.bl_idname, text="Wavefront (.obj)")
 
 
 classes = (
     ImportOBJ,
-    OBJ_PT_import_include,
-    OBJ_PT_import_transform,
-    OBJ_PT_import_geometry,
     ExportOBJ,
-    OBJ_PT_export_include,
-    OBJ_PT_export_transform,
-    OBJ_PT_export_geometry,
 )
 
 
@@ -504,13 +336,13 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+    bpy.types.INFO_MT_file_import.append(menu_func_import)
+    bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+    bpy.types.INFO_MT_file_import.remove(menu_func_import)
+    bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
     for cls in classes:
         bpy.utils.unregister_class(cls)
